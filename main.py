535bbc6af30b92812284a2b615b682c140233bc9
Loïc Delpierre <loic.delpierre16@gmail.com>
2023-10-25 14:04:03 +0200

    fix: put default value

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 @app.get("/products/{token}")
-def read_product(token : int, q: Union[str, None] = None):
+def read_product(token : str, q: Union[str, None] = None):
     api = OpenFoodApi().api
     product = api.product.get(token)
     return {
-        "abbreviated_product_name_fr": product['product'].get('abbreviated_product_name_fr'),
-        "generic_name": product['product'].get('product_name'),
-        "brands": product['product'].get('brands'),
+        "abbreviated_product_name_fr": product['product'].get('abbreviated_product_name_fr', "Inconnu"),
+        "generic_name": product['product'].get('product_name', "Inconnu"),
+        "brands": product['product'].get('brands', "Inconnu"),
         "images": product['product']['selected_images'].get('front'),
         "eco": product['product'].get('ecoscore_data')
     }
